95b668a92a2f45dfc051e0b2c70f89101c9c7cae
Lazaro Yovanys Carabeo Vazquez <lazaroyovanys@gmail.com>
2024-07-13 18:34:26 -0400

    security fixed

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from modules.flask_cors import CORS, cross_origin
+from dotenv import load_dotenv
+import os
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'LACHI' 
+
+    load_dotenv()
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
     app.config["SESSION_TYPE"] = "filesystem"
 
     CORS(app)
